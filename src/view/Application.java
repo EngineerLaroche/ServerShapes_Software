7d8af6b0945060b2fd85f399e42313f426404b3c
Alexandre Laroche <35577057+k1n6ar3a@users.noreply.github.com>
2020-01-16 11:19:57 -0500

    Added Application.jar

--- src/view/Application.java
+++ src/view/Application.java
@@ -21,7 +21,7 @@ public class Application{
 	public static void main(String[] args) {
 
 		//Démarrage d'une nouvelle application 
-		Application application = new Application();
+		new Application();
 	}
 
 	/********************
