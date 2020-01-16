24234acb6c486786e4fd5e6c685148c9a3450757
Alexandre Laroche <35577057+k1n6ar3a@users.noreply.github.com>
2020-01-16 08:19:02 -0500

    Added java project to new repository

---
+++ src/view/Application.java
@@ -0,0 +1,44 @@
+
+package view;
+
+import controller.CommBase;
+
+/*******************************************************************
+
+ * @Titre_Classe: DÉMARRAGE APPLICATION
+ * 
+ * @Résumer:
+ * Représente l'application dans son ensemble.
+ * On créé une nouvelle Base de Communication avec des listeners 
+ * et une nouvelle fenêtre, qui est celle l'application. 
+ * 
+ * @Auteur: Alexandre Laroche
+ * @Date: 08 mai 2017 
+ * 
+ *******************************************************************/
+public class Application{
+
+	public static void main(String[] args) {
+
+		//Démarrage d'une nouvelle application 
+		Application application = new Application();
+	}
+
+	/********************
+	 * Constructeur
+	 ********************/
+	public Application(){
+
+		//Démarre un nouveau processus de communication
+		CommBase comm = new CommBase();
+
+		//Démarre une nouvelle fenêtre d'affichage
+		FenetrePrincipale fenetre = new FenetrePrincipale(comm);
+
+		//Interruption connexion si la fenêtre principale est fermé
+		comm.setPropertyChangeListener(fenetre);
+		
+		//Permet d'envoyer un message si on ferme la fenêtre principale
+		fenetre.addWindowListener(comm); 
+	}
+}
